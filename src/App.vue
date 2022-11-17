59fd3a73651e5cef82235847a35297d160c99dc1
benitorubano1992 <rubanobenito@gmail.com>
2022-11-17 14:25:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<template>
+  <h1>Applicazione funzionante</h1>
+  <p> <i class="fa-solid fa-droplet" v-for="n in 4"></i></p>
+</template>
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
